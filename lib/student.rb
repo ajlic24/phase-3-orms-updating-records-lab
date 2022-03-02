95fc4b678275a0400d1258021fed6eacfa1c4aac
ajlic24@gmail.com <ajlic24@gmail.com>
2022-03-01 22:05:28 -0500

    commit tree 69dd4f56ed31e175687008178043755d3540b0b2

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,62 @@
 require_relative "../config/environment.rb"
 
 class Student
+  attr_accessor :name, :grade
+  attr_reader :id
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+  def initialize(name, grade, id = nil)
+    @name = name 
+    @grade = grade 
+    @id =id
+  end
 
+  def self.create_table
+    sql = "CREATE TABLE IF NOT EXISTS students(
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER);"
+  
+    DB[:conn].execute(sql)
+  end
 
+  def self.drop_table
+    DB[:conn].execute("DROP TABLE students")
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+      sql = "INSERT INTO students(name, grade) VALUES(?,?)"    
+    
+      DB[:conn].execute(sql, self.name, self.grade)
+
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end
+
+  def self.create(name, grade)
+    new_instance = self.new(name, grade)
+    new_instance.save    
+  end
+
+  def update
+    sql = <<-SQL
+      UPDATE students
+      SET name = ?, grade = ?
+      WHERE id = ?;
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
+
+  def self.new_from_db(row)
+    self.new(name = row[1], grade = row[2], id = row[0])
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ?;"
+
+    found = DB[:conn].execute(sql, name)[0]
+    self.new(name = found[1], grade = found[2], id = found[0])
+  end
 end
